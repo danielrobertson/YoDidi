62f6a11ee36d5d476b67d013f45c24803072ffff
danielrobertson <danielrobertson733@gmail.com>
2017-04-29 23:31:13 -0500

    converted img to base64 bytes

--- YourCard/ViewController.swift
+++ YourCard/ViewController.swift
@@ -12,41 +12,21 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     
     // Take image
     @IBAction func takePhoto(_ sender: UIButton) {
-        print("*****takePhoto")
+        print("takePhoto")
         imagePicker =  UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         present(imagePicker, animated: true, completion: nil)
     }
-    
-    // Saving Image here
-    @IBAction func save(_ sender: AnyObject) {
-        print("*****save")
-        print(imageTake)
-        
-    }
-    
-    //MARK: - Add image to Library
-    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("*****image")
-        if let error = error {
-            // we got back an error!
-            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-        } else {
-            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
-        }
-    }
-    
-    //MARK: - Done image capture here
+
+    // Process image
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        print("*****imagePickerController")
+        print("imagePickerController")
         imagePicker.dismiss(animated: true, completion: nil)
         let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
         print("Got Image ***" + pickedImage.debugDescription)
+        let imageData:NSData = UIImagePNGRepresentation(pickedImage!)! as NSData
+        let strBase64 = imageData.base64EncodedString()
     }
     
 }
